cd4954354ac477171939bcf367b510d0be1e2f8b
BrainStarr <brian.r.starr@gmail.com>
2014-11-04 13:18:10 -0800

    added tap recognizer and corresponding method

--- Race 2 tha top/ViewController.m
+++ Race 2 tha top/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "PathView.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet PathView *pathView;
 
 @end
 
@@ -17,11 +20,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapDetected:)];
+    tapRecognizer.numberOfTapsRequired = 2;
+    [self.pathView addGestureRecognizer:tapRecognizer];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)tapDetected:(UITapGestureRecognizer *)tapRecognizer
+{
+    NSLog(@"Tap dat");
+    CGPoint tapLocation = [tapRecognizer locationInView:self.pathView];
+    NSLog(@"location at (%f, %f)", tapLocation.x, tapLocation.y);
 }
 
 @end
