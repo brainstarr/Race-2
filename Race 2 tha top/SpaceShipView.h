456b240df3d280a975144e7f2d6cdbc5a1889c36
BrainStarr <brian.r.starr@gmail.com>
2014-11-04 11:56:45 -0800

    added spaceship drawing code and VC

---
+++ Race 2 tha top/SpaceShipView.h
@@ -0,0 +1,13 @@
+//
+//  SpaceShipView.h
+//  Race 2 tha top
+//
+//  Created by Brian Starr on 11/4/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpaceShipView : UIView
+
+@end
