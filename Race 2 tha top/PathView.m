48169c8a166a7f97931c9a96883435e26a7d704b
BrainStarr <brian.r.starr@gmail.com>
2014-11-04 12:46:05 -0800

    added code for full path

--- Race 2 tha top/PathView.m
+++ Race 2 tha top/PathView.m
@@ -11,6 +11,29 @@
 
 @implementation PathView
 
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self){
+        [self setup];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        // Initialization code
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup
+{
+    self.backgroundColor = [UIColor clearColor];
+}
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
